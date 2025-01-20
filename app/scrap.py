b2f4716cf468346a898e72e9c19104e23662b59e
AIINSTARAJ <tomiwakuteyi@gmail.com>
2025-01-20 01:54:14 +0100

    'Console'

--- app/scrap.py
+++ app/scrap.py
@@ -31,10 +31,9 @@ def scrape(topic):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
 
-
     response = requests.get(url,headers=headers)
 
-    time.sleep(15)
+    time.sleep(0)
 
     soup = BeautifulSoup(response.text,"html.parser")
     papers = soup.find_all("div", class_ = "gs_ri")
