a51d68bda59e0d0f26c88d6824648031907a14d0
AIINSTARAJ <tomiwakuteyi@gmail.com>
2025-06-21 22:45:09 +0100

    Database Instance Changed

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ db_path = os.path.join(basedir, '..', 'src', 'assets', 'data', 'data.db')
 try:
     checker = requests.get('https://google.com')
     if checker.status_code == 200:
-        SQLALCHEMY_DATABASE_URI = "postgresql://dynax:UpQC8z67I72voRo4qPAk2RT3wlIKUpyi@dpg-d098hcvdiees73dmcrj0-a.oregon-postgres.render.com/dynax"
+        SQLALCHEMY_DATABASE_URI = "postgresql://dynax_towi_user:lcOA6Tz5CYo5YKdtT1eCAU5y9px0H4yE@dpg-d1biebmuk2gs739qhrng-a.oregon-postgres.render.com/dynax_towi"
 except Exception as E:
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.abspath(db_path)}"
 
